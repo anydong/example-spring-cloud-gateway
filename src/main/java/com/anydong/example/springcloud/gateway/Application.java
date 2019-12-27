e6a964cacff569bda1fbd2f533ed4b13a15fa03b
liuzhaowei <liuzhaowei55@gmail.com>
2019-12-27 15:04:12 +0800

    update

---
+++ src/main/java/com/anydong/example/springcloud/gateway/Application.java
@@ -0,0 +1,23 @@
+package com.anydong.example.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/ip")
+                        .uri("https://httpbin.org"))
+                .build();
+
+    }
+}
